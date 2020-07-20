729ed2e040aec46fd21678a8da7f1f5789083088
Sheehan Olver <solver@mac.com>
2020-07-20 16:55:27 +0100

    work on lanczos

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,31 @@ using OrthogonalPolynomialsQuasi, FillArrays, LazyArrays, ArrayLayouts
 
 P = Legendre(); x = axes(P,1)
 X = P \ (x .* P)
-w = P * Vcat(Vector((P \ exp.(x))[1:20]),Zeros(∞));
+w = P * (P \ exp.(x))
 W = P \ (w .* P)
+M = P'P
+M*W
+
+W*p0
+Debugger.@enter M*p0
+
 C = zeros(∞,10)
 C[1,1] = 1
+p0 = view(C,:,1)
+γ = sqrt(dot(p0, W*p0))
+lmul!(inv(γ), p0)
+
+
 v = view(C,:,2)
-muladd!(1.0,X, view(C,:,1), 0.0, v)
+muladd!(1.0,X, p0, 0.0, v)
+β = dot(M * p0, W * v)
+BLAS.axpy!(-β, p0, v)
+γ = sqrt(dot(v, W * v))
+lmul!(inv(γ), v)
 
-dot(view(C,:,1), W * v)
 
 
-
-
-
+M * p0
 
 C[1,1] = 1
 C[:,1]
