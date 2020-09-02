e0fd181afdc43512496e26c2703274cb23dd67cb
Sheehan Olver <solver@mac.com>
2020-09-02 17:02:43 +0100

    start semiclassical lanczos

--- src/SemiclassicalOrthogonalPolynomials.jl
+++ src/SemiclassicalOrthogonalPolynomials.jl
@@ -13,4 +13,7 @@ import ContinuumArrays: basis
 export LanczosPolynomial, Legendre, Normalized, normalize
 
 
+
+
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,14 @@
-using SemiclassicalOrthogonalPolynomials, OrthogonalPolynomialsQuasi, BandedMatrices, Test
-import OrthogonalPolynomialsQuasi: recurrencecoefficients, resizedata!
+using SemiclassicalOrthogonalPolynomials, OrthogonalPolynomialsQuasi, ContinuumArrays, BandedMatrices, Test
+
+##
+# Arc
+##
+
+x = Inclusion(0..1)
+w = sqrt.(1 .- x .^2)
+LanczosPolynomial(w)
+
+OrthogonalPolynomialsQuasi.LegendreWeight()[affine(x,axes(wP,1))]
+
+wP = WeightedJacobi(0,1/2); wP = wP[affine(x,axes(wP,1)),:]
 
