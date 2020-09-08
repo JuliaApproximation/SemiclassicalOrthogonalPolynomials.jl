feb7ea0abba3692c01218bd16c404fcdae9803b9
Sheehan Olver <solver@mac.com>
2020-09-08 13:31:10 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,8 +9,6 @@ using SemiclassicalOrthogonalPolynomials, OrthogonalPolynomialsQuasi, ContinuumA
     x = axes(P₊,1)
     y = @.(sqrt(1 - x^2))
 
-    view(y, 0.1)
-
     U = LanczosPolynomial(y, P₊)
 
     P₋ = jacobi(0,-1/2,0..1)
@@ -29,8 +27,13 @@ using SemiclassicalOrthogonalPolynomials, OrthogonalPolynomialsQuasi, ContinuumA
     J_T = jacobimatrix(T)
     
     H_1 = T
-    H_2 = BroadcastQuasiMatrix(*, y, U)
+    H_2 = y .* U
+
+    @test (T \ (y .* H_2[:,1]))[1:3] ≈ R[1,1:3]
+
     n = 5
     @test x̃ * H_1[x̃,n] ≈ dot(J_T[n-1:n+1,n],H_1[x̃,n-1:n+1])
     @test x̃ * H_2[x̃,n] ≈ dot(J_U[n-1:n+1,n],H_2[x̃,n-1:n+1])
+    @test ỹ * H_1[x̃,n] ≈ dot(R[n-2:n,n], H_2[x̃,n-2:n])
+    @test ỹ * H_2[x̃,n] ≈ (1 - x̃^2)*U[x̃,n] ≈ dot(R[n,n:n+2], H_1[x̃,n:n+2])
 end
