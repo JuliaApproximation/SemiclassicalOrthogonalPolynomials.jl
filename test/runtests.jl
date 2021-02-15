0b45c8683fef204f4991e1350403f111d27a36d7
Timon Salar Gutleb <timon.gutleb@gmail.com>
2021-02-15 14:24:16 +0000

    import functions for testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using SemiclassicalOrthogonalPolynomials, ClassicalOrthogonalPolynomials, ContinuumArrays, BandedMatrices, QuasiArrays, Test, LazyArrays, LinearAlgebra
 import BandedMatrices: _BandedMatrix
-import SemiclassicalOrthogonalPolynomials: op_lowering
+import SemiclassicalOrthogonalPolynomials: op_lowering, initialα, αcoefficients
 import ClassicalOrthogonalPolynomials: recurrencecoefficients, orthogonalityweight
 
 @testset "OrthogonalPolynomialRatio" begin
