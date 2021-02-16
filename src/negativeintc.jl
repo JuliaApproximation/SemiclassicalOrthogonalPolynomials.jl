c4eb5e70e734c4ac44a510ff790a8df7aacda4fb
Timon Salar Gutleb <timon.gutleb@gmail.com>
2021-02-16 16:24:49 +0000

    significantly simplify initial condition

--- src/negativeintc.jl
+++ src/negativeintc.jl
@@ -1,5 +1,5 @@
 # inital value n=0 for α_{n,n-1}(t) coefficients
-initialα(t) = 2*(t*acoth(t)-1)/(log1p(t)-log(t-1))
+initialα(t) = t-2/(log1p(t)-log(t-1))
 
 # takes a previously computed vector of α_{n,n-1}(t) that has been increased in size and fills in the missing data guided by indices in inds
 function αcoefficients!(α,t,inds)
