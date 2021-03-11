efea412133d49b5dfd5ca909c7702c1e96c6fb5d
Timon Salar Gutleb <timon.gutleb@gmail.com>
2021-03-11 18:24:15 +0000

    tests for jacobimatrix starting to pass

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
-using SemiclassicalOrthogonalPolynomials, ClassicalOrthogonalPolynomials, ContinuumArrays, BandedMatrices, QuasiArrays, Test, LazyArrays, LinearAlgebra
+using SemiclassicalOrthogonalPolynomials
+using ClassicalOrthogonalPolynomials, ContinuumArrays, BandedMatrices, QuasiArrays, Test, LazyArrays, LinearAlgebra
 import BandedMatrices: _BandedMatrix
-import SemiclassicalOrthogonalPolynomials: op_lowering, initialα, αcoefficients!, evalϕn, backαcoeff!, αdirect!, αdirect
+import SemiclassicalOrthogonalPolynomials: op_lowering, initialα, αcoefficients!, evalϕn, backαcoeff!, αdirect!, αdirect, converttolaplace, JacobiMatrixM1, initialjacobi,jacobiopm1extension!
 import ClassicalOrthogonalPolynomials: recurrencecoefficients, orthogonalityweight
 
 @testset "Jacobi" begin
@@ -351,7 +352,6 @@ end
     end
 end
 
-
 @testset "OPs for a=b=0, c=-1" begin
     @testset "inital α" begin
         t1 = 1.1
@@ -464,17 +464,17 @@ end
         α[1] = initialα(t)
         αcoefficients!(α,t,2:N)
         # generate B operator
-        B = BandedMatrices._BandedMatrix(Vcat((-1).^(1:N)' .* α,(-1).^(0:N-1)'), N, 0,1)
+        B = converttolaplace(t,N)
         # represent some ϕ_n(x) in Legendre polynomial basis
         n = 1
-        ϕ1 = P \ @.(α[n+1]*ClassicalOrthogonalPolynomials.jacobip(n-1,0,0,-x)+ClassicalOrthogonalPolynomials.jacobip(n,0,0,-x))
+        ϕ1 = P \ @.(α[n]*ClassicalOrthogonalPolynomials.jacobip(n-1,0,0,-x)+ClassicalOrthogonalPolynomials.jacobip(n,0,0,-x))
         n = 2
-        ϕ2 = P \ @.(α[n+1]*ClassicalOrthogonalPolynomials.jacobip(n-1,0,0,-x)+ClassicalOrthogonalPolynomials.jacobip(n,0,0,-x))
+        ϕ2 = P \ @.(α[n]*ClassicalOrthogonalPolynomials.jacobip(n-1,0,0,-x)+ClassicalOrthogonalPolynomials.jacobip(n,0,0,-x))
         # test B operator consistency
         @test ϕ1[1:N] ≈ B[1:N,2]
-        @test (B \ ϕ1[1:N])[2] ≈ 1
+        @test (qr(B) \ ϕ1[1:N])[2] ≈ 1
         @test ϕ2[1:N]≈ B[1:N,3]
-        @test (B \ ϕ2[1:N])[3] ≈ 1
+        @test (qr(B) \ ϕ2[1:N])[3] ≈ 1
     end
 
     @testset "OPs for a=b=0, c=-1 - basic evaluation consistency" begin
@@ -488,19 +488,57 @@ end
         α[1] = initialα(t)
         αcoefficients!(α,t,2:N)
         # generate B operator
-        B = BandedMatrices._BandedMatrix(Vcat((-1).^(1:N)' .* α,(-1).^(0:N-1)'), N, 0,1)
+        B = converttolaplace(t,N)
         # some test functions
         f1(x) = x^2
         f2(x) = (t-x)^2
         f3(x) = exp(t-x)
         # test basic expansion and evaluation via Legendre()
         y = 2*rand(1)[1]-1
-        u1 = B \ (P[:,1:20] \ f1.(x))
+        u1 = qr(B) \ (P[:,1:20] \ f1.(x))
         @test (P[:,1:20]*(B*u1))[y] ≈ f1(y)
-        u2 = B \ (P[:,1:20] \ f2.(x))
+        u2 = qr(B) \ (P[:,1:20] \ f2.(x))
         @test (P[:,1:20]*(B*u2))[y] ≈ f2(y)
-        u3 = B \ (P[:,1:20] \ f3.(x))
+        u3 = qr(B) \ (P[:,1:20] \ f3.(x))
         @test (P[:,1:20]*(B*u3))[y] ≈ f3(y)
+    end
+
+    @testset "OPs for a=b=0, c=-1 - Jacobi matrix basics" begin
+        t = BigFloat("1.2")
+        Jinf = JacobiMatrixM1(t)
+        Jinit = initialjacobi(t,20)
+        Jfill = jacobiopm1extension!(initialjacobi(t,20),Array(10:20),t)
+        @test isbanded(Jinf.data)
+        @test isbanded(Jinit)
+        @test isbanded(Jfill)
+        @test Jfill ≈ Jinit
+        # still seeing some odd getindex behavior, for now this works
+        for j = 1:20
+            for i = 1:20
+                @test Jinf[i,j] ≈ Jinit[i,j]
+            end
+        end
+    end
+    
+    @testset "OPs for a=b=0, c=-1 - Jacobi matrix application" begin
+        # basis
+        t = BigFloat("1.1")
+        P = Legendre{BigFloat}()
+        x = axes(P,1)
+        # generate jacobi matrix
+        J = JacobiMatrixM1(t)[1:20,1:20]
+        f1(x) = x^2
+        f2(x) = (t-x)^2
+        f3(x) = exp(t-x)
+        B = converttolaplace(t,20)
+        # test basic multiplication with x and via Legendre()
+        y = BigFloat("0.817320")
+        u1 = qr(B) \ (P[:,1:20] \ f1.(x))
+        @test (P[:,1:20]*(B*J*u1))[y] - y*f1(y)  <= 1e-30
+        u2 = qr(B) \ (P[:,1:20] \ (t.-x).^2)
+        @test (P[:,1:20]*(B*J*u2))[y] - y*f2(y)  <= 1e-30
+        u3 = qr(B) \ (P[:,1:20] \ exp.(t.-x))
+        @test (P[:,1:20]*(B*J*u3))[y] - y*f3(y)  <= 1e-20
     end
 end
 
