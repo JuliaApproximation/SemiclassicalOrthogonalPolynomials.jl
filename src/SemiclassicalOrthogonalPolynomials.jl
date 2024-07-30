7739bed4f9f1732ef95db530588deca3e699385b
DanielVandH <danj.vandenheuvel@gmail.com>
2024-07-30 12:36:21 +0100

    Inefficient

--- src/SemiclassicalOrthogonalPolynomials.jl
+++ src/SemiclassicalOrthogonalPolynomials.jl
@@ -181,7 +181,7 @@ function semiclassical_jacobimatrix(Q::SemiclassicalJacobi, a, b, c)
     elseif b == -one(eltype(Q.t))
         return semiclassical_jacobimatrix(Q.t, a, b, c)
     elseif Q.b == -one(eltype(Q.t))
-        newQ = SemiclassicalJacobi(Q.t, Q.a, one(Q.b), Q.c)
+        newQ = SemiclassicalJacobi(Q.t, Q.a, one(Q.b), Q.c, Q)
         return semiclassical_jacobimatrix(newQ, a, b, c)
     end
 
